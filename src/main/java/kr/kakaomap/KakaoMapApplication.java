451883cf398ebe9ebe7b065a07c580c3ef7aebb2
msk226 <kminseok5167@gmail.com>
2025-05-25 15:28:54 +0900

    feat: Feign 클라이언트 활성화

--- src/main/java/kr/kakaomap/KakaoMapApplication.java
+++ src/main/java/kr/kakaomap/KakaoMapApplication.java
@@ -2,7 +2,9 @@ package kr.kakaomap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients // Feign 클라이언트 활성화
 @SpringBootApplication
 public class KakaoMapApplication {
 

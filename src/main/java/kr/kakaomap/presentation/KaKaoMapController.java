64681cfe4f87f42e4a1aacbca53b680d400262b8
msk226 <kminseok5167@gmail.com>
2025-05-25 15:29:43 +0900

    feat: 카카오 맵 요청 API 엔드포인트 정의

--- src/main/java/kr/kakaomap/presentation/KaKaoMapController.java
+++ src/main/java/kr/kakaomap/presentation/KaKaoMapController.java
@@ -1,17 +1,25 @@
 package kr.kakaomap.presentation;
 
+import kr.kakaomap.application.PlaceSearchService;
+import kr.kakaomap.presentation.dto.KaKaoSearchResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class KaKaoMapController {
 
+    private final PlaceSearchService placeSearchService;
+
     @GetMapping("/api/health-check")
     public String healthCheck() {
         return "Kakao Map API is running!";
     }
 
-
+    @GetMapping("/search")
+    public KaKaoSearchResponse search(@RequestParam String keyword) {
+        return placeSearchService.search(keyword);
+    }
 }
